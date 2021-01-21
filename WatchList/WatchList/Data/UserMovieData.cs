ddbbad4e0045bdcee32c0bda883d44437230acae
Jeroen Vrijkorte <jeroen@jeroenvrijkorte.nl>
2021-01-21 11:02:38 +0100

    Added display of user movie data

---
+++ WatchList/WatchList/Data/UserMovieData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WatchList.Data
+{
+    /// <summary>
+    /// User-specific data about a particular movie.
+    /// </summary>
+    public class UserMovieData
+    {
+        public bool Watched { get; set; }
+
+        /// <summary>
+        /// Movie Rating 1-10 or null.
+        /// </summary>
+        public int? Rating { get; set; }
+
+        public bool WantToWatch { get; set; }
+    }
+}
--- WatchList/WatchList/Pages/Movie.cshtml.cs
+++ WatchList/WatchList/Pages/Movie.cshtml.cs
@@ -15,6 +15,8 @@ namespace WatchList.Pages
 
         public MovieData MovieData { get; set; }
 
+        public UserMovieData UserMovieData { get; set; }
+
         private readonly IMovieRepository _movieRepository;
 
         public MovieModel(IMovieRepository movieRepository)
