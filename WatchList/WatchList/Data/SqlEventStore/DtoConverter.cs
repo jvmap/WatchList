4ccd396b57a80db875ffc4ff15a644e8558673b9
Jeroen Vrijkorte <jeroen@jeroenvrijkorte.nl>
2021-02-14 21:55:18 +0100

    Moved (de)serialization logic from SqlEventStore out to new class DtoConverter.

---
+++ WatchList/WatchList/Data/SqlEventStore/DtoConverter.cs
@@ -0,0 +1,72 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WatchList.Domain.Events;
+
+namespace WatchList.Data.SqlEventStore
+{
+    public class DtoConverter
+    {
+        public EventDto ToDto(Event evt)
+        {
+            return new EventDto
+            {
+                AggregateId = evt.AggregateId,
+                Name = GetEventName(evt),
+                EventData = SerializeEventData(evt),
+                TimeStamp = evt.Timestamp
+            };
+        }
+
+        public Event FromDto(EventDto evtDto)
+        {
+            string typeName = "WatchList.Domain.Events." + evtDto.Name + "Event,WatchList.Domain";
+            Type eventType = Type.GetType(typeName, throwOnError: true);
+            Event evt = (Event)Activator.CreateInstance(eventType);
+            eventType
+                .GetProperty(nameof(evt.AggregateId))
+                .SetValue(evt, evtDto.AggregateId);
+            eventType
+                .GetProperty(nameof(evt.Timestamp))
+                .SetValue(evt, evtDto.TimeStamp);
+            PopulateEventData(evtDto.EventData, evt);
+            return evt;
+        }
+
+        private static string GetEventName(Event evt)
+        {
+            string name = evt.GetType().Name;
+            if (!name.EndsWith("Event"))
+                throw new ArgumentException("Event classnames must end with \"Event\". Got: " + evt.GetType().Name);
+            name = name[0..^5];
+            return name;
+        }
+
+        private static string SerializeEventData(Event evt)
+        {
+            var dict = evt
+                .GetType()
+                .GetProperties()
+                .Where(p => p.DeclaringType != typeof(Event))
+                .ToDictionary(p => ToCamelCase(p.Name), p => p.GetValue(evt));
+            return JsonConvert.SerializeObject(dict);
+        }
+
+        private static void PopulateEventData(string eventData, Event evt)
+        {
+            JsonConvert.PopulateObject(eventData, evt);
+        }
+
+        private static string ToCamelCase(string name)
+        {
+            if (string.IsNullOrEmpty(name))
+                return name;
+            else if (Char.IsLower(name[0]))
+                return name;
+            else
+                return Char.ToLower(name[0]) + name.Substring(1);
+        }
+    }
+}
--- WatchList/WatchList/Data/SqlEventStore/SqlEventStore.cs
+++ WatchList/WatchList/Data/SqlEventStore/SqlEventStore.cs
@@ -14,11 +14,13 @@ namespace WatchList.Data.SqlEventStore
     public class SqlEventStore : IEventStore
     {
         private readonly DbContextOptions<SqlEventStoreDbContext> _options;
+        private readonly DtoConverter _dtoConverter;
 
         public SqlEventStore(DbContextOptions<SqlEventStoreDbContext> options)
         {
             this._options = options;
             UpgradeDatabase();
+            this._dtoConverter = new DtoConverter();
         }
 
         private void UpgradeDatabase()
@@ -35,13 +37,9 @@ namespace WatchList.Data.SqlEventStore
             {
                 foreach (Event evt in evts)
                 {
-                    db.Events.Add(new EventDto
-                    {
-                        AggregateId = evt.AggregateId,
-                        Name = GetEventName(evt),
-                        EventData = SerializeEventData(evt),
-                        TimeStamp = DateTimeOffset.Now
-                    });
+                    EventDto dto = _dtoConverter.ToDto(evt);
+                    dto.TimeStamp = DateTimeOffset.Now;
+                    db.Events.Add(dto);
                 }
                 await db.SaveChangesAsync();
             }
@@ -69,57 +67,8 @@ namespace WatchList.Data.SqlEventStore
                 dtos = await query.ToListAsync();
             }
             return dtos
-                .Select(evt => ConstructEvent(evt))
+                .Select(evt => _dtoConverter.FromDto(evt))
                 .ToList();
-        }
-
-        private static string GetEventName(Event evt)
-        {
-            string name = evt.GetType().Name;
-            if (!name.EndsWith("Event"))
-                throw new ArgumentException("Event classnames must end with \"Event\". Got: " + evt.GetType().Name);
-            name = name[0..^5];
-            return name;
-        }
-
-        private static string SerializeEventData(Event evt)
-        {
-            var dict = evt
-                .GetType()
-                .GetProperties()
-                .Where(p => p.DeclaringType != typeof(Event))
-                .ToDictionary(p => ToCamelCase(p.Name), p => p.GetValue(evt));
-            return JsonConvert.SerializeObject(dict);
-        }
-
-        private static string ToCamelCase(string name)
-        {
-            if (string.IsNullOrEmpty(name))
-                return name;
-            else if (Char.IsLower(name[0]))
-                return name;
-            else
-                return Char.ToLower(name[0]) + name.Substring(1);
-        }
-
-        private Event ConstructEvent(EventDto evtDto)
-        {
-            string typeName = "WatchList.Domain.Events." + evtDto.Name + "Event,WatchList.Domain";
-            Type eventType = Type.GetType(typeName, throwOnError: true);
-            Event evt = (Event)Activator.CreateInstance(eventType);
-            eventType
-                .GetProperty(nameof(evt.AggregateId))
-                .SetValue(evt, evtDto.AggregateId);
-            eventType
-                .GetProperty(nameof(evt.Timestamp))
-                .SetValue(evt, evtDto.TimeStamp);
-            PopulateEventData(evtDto.EventData, evt);
-            return evt;
-        }
-
-        private static void PopulateEventData(string eventData, Event evt)
-        {
-            JsonConvert.PopulateObject(eventData, evt);
         }
     }
 }
