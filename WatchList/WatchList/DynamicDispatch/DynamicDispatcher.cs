0b6c464b64d20896c41b40b7d0690d8ef14438e3
Jeroen Vrijkorte <jeroen@jeroenvrijkorte.nl>
2021-02-15 22:48:19 +0100

    Performance: cache MethodInfos in DynamicDispatcher

--- WatchList/WatchList/DynamicDispatch/DynamicDispatcher.cs
+++ WatchList/WatchList/DynamicDispatch/DynamicDispatcher.cs
@@ -3,15 +3,58 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
+using WatchList.Services;
 
 namespace WatchList.DynamicDispatch
 {
     public class DynamicDispatcher
     {
+        Memoizator<(Type targetType, string methodName, Type parameterType, Type resultType), MethodInfo> _methodInfoMemo;
+        
         private enum DispatchType
         {
             Required,
             Optional
+        }
+
+        public DynamicDispatcher()
+        {
+            _methodInfoMemo = new Memoizator<(Type targetType, string methodName, Type parameterType, Type resultType), MethodInfo>(
+                @in => GetMethodInfo(@in.targetType, @in.methodName, @in.parameterType, @in.resultType));
+
+            static MethodInfo GetMethodInfo(Type targetType, string methodName, Type parameterType, Type resultType)
+            {
+                MethodInfo[] matches = targetType
+                                .GetMethods()
+                                .Where(method => Matches(method, methodName, parameterType, resultType))
+                                .ToArray();
+                if (matches.Length > 1)
+                    throw new InvalidOperationException(
+                        $"Object of type {targetType} has more than one method {resultType} {methodName}({parameterType})" +
+                        $", where exactly one was expected.");
+                return matches.SingleOrDefault();
+            }
+
+            static bool Matches(
+                MethodInfo method,
+                string methodName,
+                Type paramType,
+                Type expectedReturnType)
+            {
+                if (method.Name == methodName
+                    && method.ReturnType == expectedReturnType)
+                {
+                    return HasSingleMatchingParameter(method, paramType);
+                }
+                return false;
+            }
+
+            static bool HasSingleMatchingParameter(MethodInfo method, Type paramType)
+            {
+                ParameterInfo[] @params = method.GetParameters();
+                return @params.Length == 1
+                    && @params[0].ParameterType == paramType;
+            }
         }
 
         public void Dispatch(object target, string methodName, object parameter)
@@ -33,7 +76,7 @@ namespace WatchList.DynamicDispatch
             return (TResult)PrivateDispatch(target, methodName, parameter, typeof(TResult), DispatchType.Optional, defaultValue);
         }
 
-        private static object PrivateDispatch(
+        private object PrivateDispatch(
             object target, 
             string methodName, 
             object parameter, 
@@ -41,8 +84,8 @@ namespace WatchList.DynamicDispatch
             DispatchType dispatchType,
             object defaultValue = null)
         {
-            MethodInfo[] methods = GetApplicableMethods(target, methodName, parameter, resultType);
-            if (methods.Length == 0)
+            MethodInfo method = _methodInfoMemo.GetValue((target.GetType(), methodName, parameter.GetType(), resultType));
+            if (method == null)
             {
                 if (dispatchType == DispatchType.Required)
                     throw new InvalidOperationException(
@@ -51,52 +94,8 @@ namespace WatchList.DynamicDispatch
                 else
                     return defaultValue;
             }
-            else if (methods.Length == 1)
-                return InvokeMethod(target, methods[0], parameter);
             else
-                throw new InvalidOperationException(
-                    $"Object of type {target.GetType()} has more than one method {resultType} {methodName}({parameter.GetType()})" +
-                    $", where exactly one was expected.");
-        }
-
-        private static MethodInfo[] GetApplicableMethods(
-            object target,
-            string methodName,
-            object parameter,
-            Type resultType)
-        {
-            Type paramType = parameter.GetType();
-            Type targetType = target.GetType();
-            return targetType
-                .GetMethods()
-                .Where(method => Matches(method, methodName, paramType, resultType))
-                .ToArray();
-        }
-
-        private static bool Matches(
-            MethodInfo method,
-            string methodName,
-            Type paramType,
-            Type expectedReturnType)
-        {
-            if (method.Name == methodName
-                && method.ReturnType == expectedReturnType)
-            {
-                return HasSingleMatchingParameter(method, paramType);
-            }
-            return false;
-        }
-
-        private static bool HasSingleMatchingParameter(MethodInfo method, Type paramType)
-        {
-            ParameterInfo[] @params = method.GetParameters();
-            return @params.Length == 1
-                && @params[0].ParameterType == paramType;
-        }
-
-        private static object InvokeMethod(object target, MethodInfo method, object argument)
-        {
-            return method.Invoke(target, new object[] { argument });
+                return method.Invoke(target, new object[] { parameter });
         }
     }
 }
---
+++ WatchList/WatchList/Services/Memoizator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WatchList.Services
+{
+    public class Memoizator<TInput, TOutput>
+    {
+        readonly Func<TInput, TOutput> _generator;
+        readonly ConcurrentDictionary<TInput, TOutput> _cache;
+
+        public Memoizator(Func<TInput, TOutput> generator)
+        {
+            this._generator = generator;
+            this._cache = new ConcurrentDictionary<TInput, TOutput>();
+        }
+
+        public Memoizator(Func<TInput, TOutput> generator, IEqualityComparer<TInput> comparer)
+        {
+            this._generator = generator;
+            this._cache = new ConcurrentDictionary<TInput, TOutput>(comparer);
+        }
+
+        public TOutput GetValue(TInput input)
+        {
+            return _cache.GetOrAdd(input, _generator);
+        }
+    }
+}
